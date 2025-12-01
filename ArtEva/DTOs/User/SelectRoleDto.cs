5284a11cadeb2162c02714579df8e21ecb63d7e8
minasalah555 <salahmina452@gmail.com>
2025-12-01 16:22:53 +0200

    init

---
+++ ArtEva/DTOs/User/SelectRoleDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtEva.DTOs.User
+{
+    public class SelectRoleDto
+    {
+        [Required(ErrorMessage = "Role is required")]
+        public UserRole Role { get; set; }
+    }
+
+    public enum UserRole
+    {
+        Buyer = 1,
+        Seller = 2
+    }
+}
